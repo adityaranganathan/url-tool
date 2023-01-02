d8481b66e99ca1e909e6a0108fbd85275c4c4de1
Aditya Ranganathan <aditya@leanstation.com>
2023-01-03 00:22:41 +0530

    Add functionality to display response body size of provided urls

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sort"
+)
 
 func main() {
-	fmt.Println("Hello")
+	urls := os.Args[1:]
+
+	DisplayURLInfo(GetURLInfo(urls))
+}
+
+type URLInfo struct {
+	URL      string
+	BodySize int
+}
+
+func GetURLInfo(urls []string) []URLInfo {
+	var result []URLInfo
+	for _, url := range urls {
+		size, err := GetResponseBodySize(url)
+		if err != nil {
+			log.Printf(err.Error())
+			continue
+		}
+
+		result = append(result, URLInfo{
+			URL:      url,
+			BodySize: size,
+		})
+	}
+
+	return result
+}
+
+func GetResponseBodySize(url string) (int, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return 0, fmt.Errorf("fetching url response of %s: %w", url, err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, fmt.Errorf("reading response body: %w", err)
+	}
+
+	return len(body), nil
+}
+
+func DisplayURLInfo(data []URLInfo) {
+	sort.Slice(data, func(i, j int) bool {
+		return data[i].BodySize < data[j].BodySize
+	})
+
+	fmt.Println("Size(bytes) \t URL")
+	for _, info := range data {
+		fmt.Println(fmt.Sprintf("%d \t\t %s", info.BodySize, info.URL))
+	}
 }
